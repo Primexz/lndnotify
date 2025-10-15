0ca59711efee82db54f286e591f4a5fcc3be31aa
Primexz <johann@pepebot.info>
2025-10-15 21:32:43 +0200

    chore: remove duplicate code

--- internal/app/app.go
+++ internal/app/app.go
@@ -10,18 +10,9 @@ import (
 	"github.com/Primexz/lndnotify/internal/lnd"
 	"github.com/Primexz/lndnotify/internal/notify"
 	log "github.com/sirupsen/logrus"
-	prefixed "github.com/x-cray/logrus-prefixed-formatter"
 )
 
 func Run(configPath string) {
-	log.SetFormatter(&prefixed.TextFormatter{
-		TimestampFormat:  "2006/01/02 - 15:04:05",
-		FullTimestamp:    true,
-		QuoteEmptyFields: true,
-		SpacePadding:     45,
-	})
-
-	log.SetReportCaller(true)
 	log.SetLevel(log.DebugLevel)
 
 	// Load configuration
